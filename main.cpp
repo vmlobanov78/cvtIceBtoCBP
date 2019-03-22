a480af8d9eed872bdfd679db5cc27bf943705867
vmlobanov78 <37369729+vmlobanov78@users.noreply.github.com>
2019-03-22 17:14:35 +0700

    Изменение 2019-03-22
    Изменение 2019-03-22 (исполнение команды системы и обработка результатов)

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[])
 {
 	//getchar();
 	string _prog = "icebw";
-	string _version = "13_8";
-	string _path_gr = _path_merge("C:", "msys2", "Projects", "iceb");
+	string _version = "14_4";
+	string _path_gr = _path_merge("C:", "msys64", "Projects", "iceb");
 	cbProject prj;
 	prj.prjPath = _path_merge(_path_gr, _prog + "-" + _version);
 	prj.buildPath = "devel";
@@ -27,3 +27,4 @@ int main(int argc, char* argv[])
 
     return 0;
 }
+
